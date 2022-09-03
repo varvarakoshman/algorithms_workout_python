0f427547d73d678ea680ff34afbcd147326c7245
vkoschman <207955@niuitmo.ru>
2022-09-03 23:32:03 +0300

    algoexpert: refactoring of easy solutions

--- algoExpert/easy/CaesarCipherEncryption.py
+++ algoExpert/easy/CaesarCipherEncryption.py
@@ -1,21 +1,55 @@
-alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+# Solution 1 (without Unicode built-in functions)
+alphabet = list("abcdefghijklmnopqrstuvwxyz")
 
 
-def caesar_cipher_encryptor(string, key):
+def caesar_cipher_encryptor_1(string, key):
     key = key % len(alphabet)
-    shifted_str = ""
+    shifted_str = []
     for char in string:
-        shifted_str += get_shifted_char(char, key)
-    return shifted_str
+        shifted_str.append(get_shifted_char_1(char, key))
+    return "".join(shifted_str)
 
 
-def get_shifted_char(char, key):
+def get_shifted_char_1(char, key):
     new_index = alphabet.index(char) + key
     if new_index > len(alphabet) - 1:
         return alphabet[new_index - len(alphabet)]
     else:
         return alphabet[new_index]
 
+    # complexity: O(n)
+    # space complexity: O(n)
+
+
+# Solution 2 (has same time & space complexities, but utilizes built-in functions)
+# Unicode:
+# z => 122
+# a => 97
+def caesar_cipher_encryptor(string, key):
+    key = key % 26
+    shifted_str = []
+    for char in string:
+        shifted_str.append(get_shifted_char(char, key))
+    return "".join(shifted_str)
+
+
+def get_shifted_char(char, key):
+    new_index = ord(char) + key
+    if new_index > 122:
+        return chr(96 + new_index % 122)
+    else:
+        return chr(new_index)
+
+    # complexity: O(n)
+    # space complexity: O(n)
+
 
 if __name__ == '__main__':
     assert caesar_cipher_encryptor("xyz", 2) == "zab"
+    assert caesar_cipher_encryptor("xyz", 5) == "cde"
+    assert caesar_cipher_encryptor("abc", 3) == "def"
+    assert caesar_cipher_encryptor("abc", 0) == "abc"
+    assert caesar_cipher_encryptor("abc", 26) == "abc"
+    assert caesar_cipher_encryptor("abc", 52) == "abc"
+    assert caesar_cipher_encryptor("abc", 57) == "fgh"
+    assert caesar_cipher_encryptor("xyz", 25) == "wxy"
--- algoExpert/easy/Run-lengthEncoding.py
+++ algoExpert/easy/Run-lengthEncoding.py
@@ -1,35 +1,17 @@
 def run_length_encoding(string):
-    index_char_mapping = get_chars_mapping(string)
-    output = str()
-    indices = list(index_char_mapping.keys())
-    for i in range(1, len(indices)):
-        cur_len = indices[i] - indices[i - 1]
-        char = index_char_mapping[indices[i - 1]]
-        output += get_new_seq(char, cur_len)
-    last_char_index = indices[-1]
-    last_char = index_char_mapping[last_char_index]
-    output += get_new_seq(last_char, len(string) - last_char_index)
-    return output
-
-
-def get_chars_mapping(string):
-    index_char_mapping = {}
-    previous = ""
-    for i in range(len(string)):
-        if string[i] != previous:
-            previous = string[i]
-            index_char_mapping[i] = previous
-    return index_char_mapping
-
-
-def get_new_seq(char, cur_len):
-    if cur_len <= 9:
-        return str(cur_len) + char
-    else:
-        full_seq_num = cur_len // 9
-        remaining_num = cur_len % 9
-        single_full_seq = "9" + char
-        return single_full_seq * full_seq_num + str(remaining_num) + char
+    curr_count = 1
+    output = []
+    for i in range(1, len(string)):
+        current_char = string[i]
+        previous = string[i - 1]
+        if current_char != previous or curr_count == 9:
+            output.append(str(curr_count))
+            output.append(str(previous))
+            curr_count = 0
+        curr_count += 1
+    output.append(str(curr_count))
+    output.append(str(string[-1]))
+    return ''.join(output)
 
     # complexity: O(n)
     # space complexity: O(n)
