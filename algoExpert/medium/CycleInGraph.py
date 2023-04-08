06f1c832f2f695b14792138cee5fd676145adc39
vkoschman <207955@niuitmo.ru>
2023-04-08 20:41:14 +0300

    algoexpert: 2nd solution to cycle in graph problem

--- algoExpert/medium/CycleInGraph.py
+++ algoExpert/medium/CycleInGraph.py
@@ -1,12 +1,12 @@
 import unittest
 
-
+# Solution 1 (with colors)
 # O(|V| + |E|) time | O(|V|) space
 
 WHITE, GREY, BLACK = 0, 1, 2
 
 
-def cycleInGraph(edges):
+def cycleInGraph_(edges):
     number_of_nodes = len(edges)
     visited = [WHITE] * number_of_nodes
     for vertex in range(number_of_nodes):
@@ -26,12 +26,47 @@ def dfs(vertex, edges, visited):
     for neighbour in neighbours:
         if visited[neighbour] == GREY:
             return True
-        if visited[neighbour] != WHITE:
+        if visited[neighbour] == BLACK:
             continue
         has_cycle = dfs(neighbour, edges, visited)
         if has_cycle:
             return True
     visited[vertex] = BLACK
+    return False
+
+
+# Solution 2 (with stack)
+# O(|V| + |E|) time | O(|V|) space
+def cycleInGraph(edges):
+    number_of_nodes = len(edges)
+    visited = [False for _ in range(number_of_nodes)]
+    currently_in_stack = [False for _ in range(number_of_nodes)]
+
+    for node in range(number_of_nodes):
+        if visited[node]:
+            continue
+
+        contains_cycle = is_node_in_cycle(node, edges, visited, currently_in_stack)
+        if contains_cycle:
+            return True
+
+    return False
+
+
+def is_node_in_cycle(node, edges, visited, currently_in_stack):
+    visited[node] = True
+    currently_in_stack[node] = True
+
+    neighbours = edges[node]
+    for neighbour in neighbours:
+        if not visited[neighbour]:
+            contains_cycle = is_node_in_cycle(neighbour, edges, visited, currently_in_stack)
+            if contains_cycle:
+                return True
+        elif currently_in_stack[neighbour]:
+            return True
+
+    currently_in_stack[node] = False
     return False
 
 
@@ -42,21 +77,25 @@ class TestProgram(unittest.TestCase):
         actual = cycleInGraph(input)
         self.assertEqual(actual, expected)
 
+    def test_case_2(self):
         input = [[1, 2], [2], []]
         expected = False
         actual = cycleInGraph(input)
         self.assertEqual(actual, expected)
 
+    def test_case_3(self):
         input = [[1, 2], [2], [1]]
         expected = True
         actual = cycleInGraph(input)
         self.assertEqual(actual, expected)
 
+    def test_case_4(self):
         input = [[0], [1]]
         expected = True
         actual = cycleInGraph(input)
         self.assertEqual(actual, expected)
 
+    def test_case_5(self):
         input = [[0]]
         expected = True
         actual = cycleInGraph(input)
